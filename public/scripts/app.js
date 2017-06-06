0c166380e7e8bca464631262341c7f65d01f34b9
vivienne white <vivihansf@gmail.com>
2017-06-06 14:47:36 -0500

    saves final changes on sprint3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,28 +10,33 @@ angular
   // ^ the first argument is a string naming the controller,
   // the second argument is a function that defines the capacities
   // of the controller.
+  AlbumsIndexController.$inject = ['$http'];
 
-function AlbumsIndexController () {
-  var vm = this;
-  vm.newAlbum = {};
+  function AlbumsIndexController ( $http ) {
+    var vm = this;
+    $http({
+      method: 'GET',
+      url: '/api/albums'
+    }).then(function successCallback(response){
+      vm.albums = response.data;
+    }, function errorCallback(response){
+      console.log('There was an error getting the data', response);
+    });
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
+    vm.createAlbum = function () {
+      $http({
+        method: 'POST',
+        url: '/api/albums',
+        data:vm.newAlbum
+      }).then(function successCallback(response){
+        console.log(response);
+
+        vm.albums.push(response.data);
+      }, function errorCallback(response){
+        console.log('There as an error posting the data', response);
+      });
+    }
+
   };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
-}
+  // create new albums
